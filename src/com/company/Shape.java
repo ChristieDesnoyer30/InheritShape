a785d064188a59f5ad1b209235f96e7be79dd331
Christie Desnoyer <christiedesnoyer@Christies-MacBook-Pro.local>
2019-02-04 09:03:00 -0500

    Setting up Main and Shape Class

---
+++ src/com/company/Shape.java
@@ -0,0 +1,42 @@
+package com.company;
+
+public class Shape {
+
+
+    private String shapeColor;
+    private boolean isFilled;
+
+    public Shape(String shapeColor, boolean isFilled) {
+
+        this.shapeColor = shapeColor;
+
+        this.isFilled = isFilled;
+    }
+
+    public String getShapeColor() {
+        return shapeColor;
+    }
+
+    public void setShapeColor(String shapeColor) {
+        this.shapeColor = shapeColor;
+    }
+
+    public boolean isFilled() {
+        return isFilled;
+    }
+
+    public void setFilled(boolean filled) {
+        isFilled = filled;
+    }
+
+    public void printShape(){
+
+        System.out.println("The shape is " + shapeColor + " in color. \n Is it filled in? " + isFilled ".");
+
+
+    }
+
+
+
+
+}
